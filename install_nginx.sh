e67bde0a3b9a1a12807a22a1fb2808125477048e
Thomas Auer <thauer@edu.aau.at>
2023-06-20 14:07:22 +0200

    Update conf to work with nginx-proxy pass

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 sudo apt install nginx
-echo "
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
+echo "server {
+  listen 80;
+  server_name sparql.devos.com;
 
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
-                proxy_pass  http://127.0.0.1:7200/;
-        }
-}
-" > /etc/nginx/sites-available/devos
+  location / {
+        proxy_pass http://localhost:7200;
+        proxy_set_header Host \$host;
+  }
+}" | sudo tee /etc/nginx/sites-available/devos.conf
+sudo unlink /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-enabled/devos.conf
+sudo ln -s /etc/nginx/sites-available/devos.conf /etc/nginx/sites-enabled/devos.conf
+sudo systemctl reload nginx
